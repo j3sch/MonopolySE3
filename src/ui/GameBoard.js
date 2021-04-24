c623dbeebe96439bc0d9acb8e591b84169866566
js414 <js414@hdm-stuttgart.de>
2021-04-24 12:37:23 +0200

    idk

--- src/ui/GameBoard.js
+++ src/ui/GameBoard.js
@@ -1,45 +1,49 @@
 import { GameFieldTop, GameFieldLeft, GameFieldRight, GameFieldBottom } from '~/ui/GameField'
 
-export function GameBoard() {
+export function GameBoard(props) {
+ const content = props.gameFields.map((gameField) => (
+   
+   <div key={gameField.id}>
+     <h3>{gameField.title}</h3>
+     <p>{gameField.price}</p>
+   </div>
+ ))
   return (
-    <div className='grid grid-cols-10 h-screen'>
-      <div className='row-start-2 row-end-10 col-start-2 col-end-10 bg-white' />
-      <div className='h-auto bg-red-600 '>dddd</div>
-      <GameFieldTop />
-      <GameFieldTop />
-      <GameFieldTop />
-      <GameFieldTop />
-      <GameFieldTop />
-      <GameFieldTop />
-      <GameFieldTop />
-      <GameFieldTop />
-      <div className='h-auto  bg-red-600' />
-      <GameFieldLeft />
-      <GameFieldRight />
-      <GameFieldLeft />
-      <GameFieldRight />
-      <GameFieldLeft />
-      <GameFieldRight />
-      <GameFieldLeft />
-      <GameFieldRight />
-      <GameFieldLeft />
-      <GameFieldRight />
-      <GameFieldLeft />
-      <GameFieldRight />
-      <GameFieldLeft />
-      <GameFieldRight />
-      <GameFieldLeft />
-      <GameFieldRight />
-      <div className='h-auto bg-red-600' />
-      <GameFieldBottom />
-      <GameFieldBottom />
-      <GameFieldBottom />
-      <GameFieldBottom />
-      <GameFieldBottom />
-      <GameFieldBottom />
-      <GameFieldBottom />
-      <GameFieldBottom />
-      <div className='h-auto bg-red-600' />
+    <div>
+      {content}
     </div>
   )
+ 
+    
+    
+    
+
+    // <div className='grid grid-cols-9 h-screen'>
+    //   <div className='row-start-2 row-end-7 col-start-3 col-end-8 bg-white' />
+    //   <div className='h-auto bg-red-600 col-span-2'></div>
+    //   <GameFieldTop name='sss' price='dssd' />
+    //   <GameFieldTop name='sss' price='dssd' />
+    //   <GameFieldTop name='sss' price='dssd' />
+    //   <GameFieldTop name='sss' price='dssd' />
+    //   <GameFieldTop name='sss' price='dssd' />
+    //   <div className='h-auto  bg-red-600 col-span-2'></div>
+    //   <GameFieldLeft />
+    //   <GameFieldRight />
+    //   <GameFieldLeft />
+    //   <GameFieldRight />
+    //   <GameFieldLeft />
+    //   <GameFieldRight />
+    //   <GameFieldLeft />
+    //   <GameFieldRight />
+    //   <GameFieldLeft />
+    //   <GameFieldRight />
+    //   <div className='h-auto bg-red-600 col-span-2' />
+    //   <GameFieldBottom />
+    //   <GameFieldBottom />
+    //   <GameFieldBottom />
+    //   <GameFieldBottom />
+    //   <GameFieldBottom />
+    //   <div className='h-auto bg-red-600 col-span-2' />
+    // </div>
+  
 }
