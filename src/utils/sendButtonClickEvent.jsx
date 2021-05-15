8bbb4e287bbd93d945746cb60f825945f1a4a75b
js414 <js414@hdm-stuttgart.de>
2021-05-15 19:10:26 +0200

    added Next player button and change the position of the buttons

--- src/utils/sendButtonClickEvent.jsx
+++
@@ -1,8 +0,0 @@
-import { PlayerContext } from '~/utils/PlayerContext';
-import { useContext } from 'react';
-
-export const sendButtonClickEvent = () => {
-	const { stompClient } = useContext(PlayerContext);
-        
-	stompClient.send('/server/buttonClicked', {}, JSON.stringify({ message : "Player has rolled a number" }));
-};
