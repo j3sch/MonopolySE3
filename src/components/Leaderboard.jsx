85d59acc2f0f81ef39a7e8c3cd4a92e8510deedd
js414 <js414@hdm-stuttgart.de>
2021-06-13 16:12:55 +0200

    hightlight current player

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -3,7 +3,7 @@ import { PlayerContext } from '~/utils/PlayerContext';
 
 // colors are missing by players
 export function Leaderboard() {
-	const { players } = useContext(PlayerContext) || {};
+	const { players, currentPlayer } = useContext(PlayerContext) || {};
 
 	return (
 		<div>
@@ -16,12 +16,20 @@ export function Leaderboard() {
 								className="grid justify-items-stretch grid-cols-6 h-16 p-1 group hover:text-2xl font-bold text-xl transition hover:p-0 hover:bg-opacity-10 "
 							>
 								<div
-									className={`bg-[${player.colour}] mr-2 group-hover:mr-1 col-span-2 xl:col-span-1 items-center grid justify-items-center`}
+									className={`bg-[${
+										player.colour
+									}] mr-2 group-hover:mr-1 col-span-2 xl:col-span-1 items-center grid justify-items-center ${
+										i === currentPlayer && 'border-2'
+									}`}
 								>
 									{i + 1}
 								</div>
 								<div
-									className={`bg-[${player.colour}] col-span-4 xl:col-span-5 grid grid-cols-2 items-center px-3 `}
+									className={`bg-[${
+										player.colour
+									}] col-span-4 xl:col-span-5 grid grid-cols-2 items-center px-3 ${
+										i === currentPlayer && 'border-2'
+									}`}
 								>
 									<div className="col-span-1 ">{player.name}</div>
 									<div className="col-span-1 justify-self-end">
