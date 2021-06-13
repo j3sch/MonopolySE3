ca2b2deac770465a286334d031c02c1d5d8f2739
js414 <js414@hdm-stuttgart.de>
2021-06-13 13:43:25 +0200

    heroku url

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -89,11 +89,15 @@ const Home = () => {
 
 	const connect = () => {
 		const sessionId = randomstring.generate(12);
-		SockJS = new SockJS('http://localhost:8080//ws-monopoly', [], {
-			sessionId: () => {
-				return sessionId;
+		SockJS = new SockJS(
+			'https://radiant-falls-52552.herokuapp.com//ws-monopoly',
+			[],
+			{
+				sessionId: () => {
+					return sessionId;
+				},
 			},
-		});
+		);
 		stompClient = Stomp.over(SockJS);
 
 		stompClient.connect({}, onConnected, onError);
